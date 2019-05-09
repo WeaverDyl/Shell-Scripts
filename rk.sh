e15a8ec52f9805e2d8628f24eff28fdf99f7c49d
Dylan Weaver <dweaver@umd.edu>
2019-05-09 12:18:09 -0400

    add messages to rk.sh

--- rk.sh
+++ rk.sh
@@ -2,7 +2,10 @@ function knit {
    Rscript -e "require(knitr); require(markdown); knit('$1.rmd', '$1.md'); markdownToHTML('$1.md', '$1.html', options=c('use_xhml'))"
 
    pandoc -s $1.html -o $1.pdf
-
+   echo "Created $1.pdf..."
+   
+   echo "...Removing extra files..."
    rm $1.md
    rm $1.html
+   echo "All done!"
 }
